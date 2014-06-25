40a62a0d8963bbfa8255abcaad11cf384106108d
Tim Spear <tim@tspear.com>
2014-06-25 19:20:34 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -45,7 +45,17 @@
     <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
     <!-- BS3 -->
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <!-- GA -->
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-135674-13', 'tspear.com');
+  ga('send', 'pageview');
+
+</script>
     <!-- WP FOOTER -->
     <?php wp_footer(); ?>
     <!-- /WP FOOTER -->
