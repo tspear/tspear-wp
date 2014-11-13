00e5158b2155eb1dbcbf4178195e708ee2e823c5
Tim Spear <tim@tspear-mbp.local>
2014-11-13 07:26:28 +0000

    adding project nav to portfolio pages

--- content.php
+++ content.php
@@ -1,18 +1,21 @@
 <?php
-/**
- * The default template for displaying content. Used for both single and index/archive/search.
+/*
+ * The default template for displaying content.
+ * Used for both single and index/archive/search.
  */
 ?>
 <!-- [CONTENT.PHP] -->
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
 	<header class="entry-header">
+	<div class="page-header">
+
 		<?php if ( is_single() ) : ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 		<?php else : ?>
-			
-			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 		<?php endif; // is_single() ?>
+		</div>
 	</header><!-- .entry-header -->
 
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
@@ -26,17 +29,6 @@
 		</div><!-- .entry-content -->
 	<?php endif; ?>
 
-	<footer class="entry-meta">
-		<?php if ( comments_open() && ! is_single() ) : ?>
-			<div class="comments-link">
-				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'twentythirteen' ) . '</span>', __( 'One comment so far', 'twentythirteen' ), __( 'View all % comments', 'twentythirteen' ) ); ?>
-			</div><!-- .comments-link -->
-		<?php endif; // comments_open() ?>
-
-		<?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
-			<?php get_template_part( 'author-bio' ); ?>
-		<?php endif; ?>
-	</footer><!-- .entry-meta -->
 </article><!-- #post -->
 
 <!-- [ / CONTENT.PHP] -->
--- single-jetpack-portfolio.php
+++ single-jetpack-portfolio.php
@@ -1,28 +1,29 @@
 <?php
-/**
+/*
  * The Template for displaying all single posts.
- *
  */
 get_header(); ?>
 	
-<script>console.log('**template: jetpacksingle.php')</script>
+<script>console.log('** template: jetpacksingle.php')</script>
 
 <div class='container'>
+	<?php /* The loop */ ?>
+	<?php while ( have_posts() ) : the_post(); ?>
 
-<?php /* The loop */ ?>
-<?php while ( have_posts() ) : the_post(); ?>
-	<?php get_template_part( 'content', get_post_format() ); ?>
-<?php endwhile; ?>
+		<?php get_template_part( 'content', get_post_format() ); ?>
+	 
+	 	<div class="taglist">
+			<div class="btn-group btn-group-sm">
+				<?php echo the_terms($post->ID,'jetpack-portfolio-tag','<button type="button" class="btn btn-default">','</button><button type="button" class="btn btn-default">','</button>'); ?>
+			</div>
+		</div>
+		
+	<?php /* End The loop */ ?>
+	<?php endwhile; ?>
 
+	<h3>More work</h3>
+	<? echo do_shortcode('[portfolio display_types=false display_tags=false display_content=false columns=6 showposts=10]'); ?>
+	<h4><?php next_posts_link('%link <span class="glyphicon glyphicon-arrow-right"></span>'); ?></h4>
 
-<h3>Taglist: <?php echo get_the_tag_list(); ?></h3>
-<?php
-	// Translators: used between list items, there is a space after the comma.
-	$tag_list = get_the_tag_list( '<span class="label label-default">', '</span><span class="label label-default">','</span>' );
-	if ( $tag_list ) {
-		echo '<h4>TAGS:' . $tag_list . '</h4>';
-	}
-?>
 </div>
-
 <?php get_footer(); ?>
