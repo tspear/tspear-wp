03e141d2bf96d40ff591b810b30f60abfcf03417
Tim Spear <tim@tspear.com>
2015-05-05 11:17:51 +0100

    2 fixes
    remove hardcoded analytics code
    add hack for jetpack portfolio

--- footer.php
+++ footer.php
@@ -37,26 +37,14 @@
             ?>
         </div>
     </footer>
-
-    <?php 
-      // only include if site is live
-      if(strpos($_SERVER["HTTP_HOST"], 'tspear.com') !== false):
-    ?>
-      <!-- ANALYTICS -->
-      <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-135674-13', 'tspear.com');
-        ga('send', 'pageview');
-      </script>
-      <?php else: ?>
-      <!-- ANALYTICS: NOT LOADED -->
-    <?php endif; ?>
-
     <!-- WP FOOTER -->
     <?php wp_footer(); ?>
     <!-- /WP FOOTER -->
+    <!-- HACK: Fix spacing on Portfolio shortcode -->
+    <style type="text/css"> 
+      .portfolio-entry {
+        margin-bottom: 1em;
+      }
+    </style>
   </body>
 </html>
