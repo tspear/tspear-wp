c82d70c3a320073246418225b98104bae3802276
Tim Spear <tim@tspear.com>
2015-05-05 15:00:37 +0100

    add taxonomy page for portfolio tags

---
+++ taxonomy-jetpack-portfolio-tag.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * @package WordPress
+ * @subpackage tspear
+ * @since tspear 1.0
+ */
+get_header(); ?>
+
+	<div class="container">
+
+		<h1><a href="<?php echo home_url( 'portfolio' );?>">Portfolio</a> - <?php single_tag_title('Projects tagged '); ?></h1>
+
+		<?php if ( have_posts() ) : ?>
+
+			<div class="row">
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div class="col-sm-6 col-md-4 portfolio-entry">
+					<?php get_template_part( 'content', 'thumbnail' ); ?>
+				</div>
+			<?php endwhile; ?>
+			</div>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+
+	<!-- DEBUGGER -->
+	<?php if(WP_DEBUG): ?>
+		<pre><?php
+			var_dump(home_url());
+			var_dump(get_bloginfo());
+		?></pre>
+	<?php endif ?>
+	<!-- / DEBUGGER -->
+
+
+	</div><!-- #container -->
+
+<?php get_footer(); ?>
